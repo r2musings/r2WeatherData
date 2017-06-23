708a05ecbbf7124013b67b45f1b43cf836b02835
r2musings <r2@r2musings.com>
2017-06-23 14:09:29 -0400

    cleaned up and removed hardcoded zip

--- server.js
+++ server.js
@@ -2,40 +2,20 @@ const request = require('request');
 var express = require('express');
 var app = express();
 let apiKey = '19bb9ff9d351e160bcb94bd5094f775f';
-let zip = '';
-let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}&units=imperial`
 
 app.get('/by-zip/:zip', function (req, res) {
-    zip = req.params.zip;
+    let zip = req.params.zip;
+    let url = `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}&units=imperial`
+
     request(url, function (err, response, body) {
         if(err){
             console.log('error:', error);
         } else {
-            // console.log('body:', body);
             res.end(body);
-            //let weather = JSON.parse(body)
-            //console.log(`It is ${weather.main.temp} in ${weather.name}.`);
         }
     });
 })
 
 var port = process.env.PORT || 1337;
 app.listen(port, function () {
-     // console.log(process.env);
-     //  var host = process.server;
-     // console.log("listening on http://%s:%s", host, port)
-     console.log("listening on http://localhost:%s", port);
 });
-
-// var http = require('http');
-
-// var server = http.createServer(function(request, response) {
-
-    // response.writeHead(200, {"Content-Type": "text/plain"});
-    // response.end("Hello World!");
-
-// });
-
-// server.listen(port);
-
-//console.log("Server running at http://localhost:%d", port);
